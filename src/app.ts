9610fe4736dbd86f17c1ab2ea84e5e356f84ebd6
carlos.zulueta <carlos.zulueta@coinscrap.com>
2025-02-09 20:36:32 +0100

    feat: added expresxs urlencoded to read x-www-form-urlencoded

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,19 @@ import dotenv from 'dotenv';
 import statusRoutes from './routes/statusRoutes';
 import carRoutes from './routes/carRoutes';
 import journeyRoutes from './routes/journeyRoutes';
+import dropoffRoutes from './routes/dropoffRoutes';
+
+
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // This lets me read x-www-form-urlencoded
 app.use(statusRoutes);
 app.use(carRoutes);
 app.use(journeyRoutes);
+app.use(dropoffRoutes);
 
 export default app;
