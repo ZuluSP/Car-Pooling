a9db09f7659ec5bd44720e0e5baa9d4ce7a5c086
carlos.zulueta <carlos.zulueta@coinscrap.com>
2025-02-17 18:19:16 +0100

    refcactor: CarService now has maps to optimize the service

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -1,11 +1,16 @@
 import { Car } from '../models/car.model';
 
-let cars: Car[] = [];
+let cars: Map<number, Car> = new Map();
 
 export const registerCars = (newCars: { id: number; seats: number }[]) => {
-  cars = newCars.map(car => ({ ...car, occupiedSeats: 0 }));
+  cars.clear();
+  newCars.forEach(car => cars.set(car.id, { ...car, occupiedSeats: 0 }));
 };
 
 export const getCars = (): Car[] => {
-  return cars;
+  return Array.from(cars.values());
+};
+
+export const getCarById = (carId: number): Car | undefined => {
+  return cars.get(carId);
 };
